3034d7304bd761e33727d26172d1e4744b775163
GianLucaCastellano <deine@email.de>
2026-04-26 18:22:40 +0200

    Started adding migration

--- api/database/migrations/20260423205029_init.ts
+++ api/database/migrations/20260423205029_init.ts
@@ -1,10 +1,18 @@
-import type { Knex } from "knex";
+import { TableBuilder, type Knex } from "knex";
 
 
 export async function up(knex: Knex): Promise<void> {
+
+    return knex.schema.createTable("notes", (table) => {
+        table.uuid("id").primary().defaultTo(knex.raw("gen_random_uuid"));
+        table.string("caption").notNullable();
+        table.string("notes").notNullable();
+        table.string("priority").notNullable();
+    })
 }
 
 
 export async function down(knex: Knex): Promise<void> {
+    return knex.schema.dropTable("notes");
 }
 
